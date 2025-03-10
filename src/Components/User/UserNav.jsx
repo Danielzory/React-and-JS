58048c218f196394834856d16d73e8e80d166858
Daniel Batista <108497953+Danielzory@users.noreply.github.com>
2025-03-10 08:01:23 -0300

    Estilização da barra de navegação do usuário. Add menú mobile

--- src/Components/User/UserNav.jsx
+++ src/Components/User/UserNav.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import {ReactComponent as MinhasFotos} from '../../Assets/feed.svg';
 import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg';
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg';
 import {ReactComponent as Sair} from '../../Assets/sair.svg';
 import Styles from './UserNav.module.css'
+import useMedia from '../../Hooks/useMedia';
 
 
 const UserNav = () => {
-    const [mobile, setMobile] = React.useState(null);
+    const mobile = useMedia('(max-width: 40rem');
     const {userLogout} = React.useContext(UserContext);
+    const [mobileMenu, setMobileMenu] = React.useState(false);
 
+    const {pathname} = useLocation();
 
+    React.useEffect(()=>{
+      setMobileMenu(false);
+    }, [pathname])
 
   return (
-    <nav className={Styles.nav}>
+  <>
+    {mobile && (
+      <button aria-label='Menu' onClick={()=>setMobileMenu(!mobileMenu)} className={`${Styles.mobileButton} ${mobileMenu && Styles.mobileButtonActive}`}></button>
+    )} 
+    <nav className={`${mobile ? Styles.navMobile : Styles.nav} ${mobileMenu && Styles.navMobileActive}`}>
       <NavLink to='/conta' end>
         <MinhasFotos />
         {mobile && "Minhas Fotos"}
@@ -33,6 +43,7 @@ const UserNav = () => {
         {mobile && "Sair"}
       </button>
     </nav>
+  </>  
   )
 }
 
