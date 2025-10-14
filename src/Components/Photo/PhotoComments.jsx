b29918824669bd967c1be4cb715b44dd64d96b60
Daniel Batista <108497953+Danielzory@users.noreply.github.com>
2025-10-14 10:44:02 -0300

    Criação dos coponentes relacionados aos comentários

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import {UserContext} from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm'
+import styles from './PhotoComment.module.css'
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(()=> props.comments || [])
+  const {login} = React.useContext(UserContext);
+
   return (
-    <div>
-      <hr></hr>
-      Comentários
-    </div>
+    <>
+      <ul className={styles.comment}>
+        {comments.map(comment => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} /> }
+    </>
   )
 }
 
